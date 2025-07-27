e9d2bc51b103286198228b4825be5b4a3ba6177f
Tousif Tasrik <83619372+lowercase-tousif@users.noreply.github.com>
2025-07-27 22:00:16 +0600

    Created: Multiplication with recursion

---
+++ Recursion/Multiplication/main.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int 
+#define ld long double
+#define endl "\n"
+#define vi vector<int>
+#define pii pair<int, int>
+
+ll multiply(int a, int b){
+	if(b == 0) return 0;
+
+	return a + multiply(a, b - 1);
+}
+
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+		
+	ll n = 1000;
+	ll m = 1000;
+
+	ll result = multiply(n, m);
+	cout << result << endl;
+}
